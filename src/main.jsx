962ff72e55bc117529f9964c94cb5bb332c5219b
Prajwal Hebbar A S <chaotic-thoughts@Prajwals-MacBook-Pro.local>
2024-08-03 20:35:37 +0530

    feat(theme): add basic theme and global styling
    - Created a basic color palette theme using createTheme and ThemeProvider.
    - Wrapped the App component inside ThemeProvider in main.jsx.
    - Added styling for the body tag to set the background in index.css.

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,30 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f6b17a",
+    },
+    secondary: {
+      main: "#f5e8c7",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "#fff",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Suspense>
-      <App />
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </ThemeProvider>
   </React.StrictMode>
 );
